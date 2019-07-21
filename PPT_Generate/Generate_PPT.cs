27e3d2622ceacf1080cf2fde4bcabf56d58a0e2f
gegegenoge7 <hayatoyokoyama@gmail.com>
2019-07-21 12:17:45 -0700

    started preliminary back-end

--- PPT_Generate/Generate_PPT.cs
+++ PPT_Generate/Generate_PPT.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Office = Microsoft.Office.Core;
+using Word = Microsoft.Office.Interop.Word;
+
 namespace PPT_Generate
 {
     class Generate_PPT
     {
-        //public void Generate(string Title, text, string OutputDirectory, string OutputFilename, string[] imageLinks)
-        //{
+        public void Generate(string Title, text, string OutputDirectory, string OutputFilename, string[] imageLinks)
+        {
 
-        //}
+        }
     }
 }
